2bdc4bcb083062f9ebc04c19526d563642d78ef2
TonSalingua <110211961+TonSalingua@users.noreply.github.com>
2022-09-14 15:31:36 +0800

    Delete src directory

--- src/About.js
+++
@@ -1,8 +0,0 @@
-
-export default function About(){
-    return(
-        <>
-            <h3>Welcome to the About Page of Student Management Portal</h3>
-        </>
-    )
-    } 
--- src/Contact.js
+++
@@ -1,8 +0,0 @@
-const Contact = ()=>{
-    return(
-        <>
-            <h3>Welcome to the Contact Page of Student Management Portal</h3>
-        </>
-    )
-    } 
-    export default Contact;
